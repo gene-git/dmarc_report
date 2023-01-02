d5162e4e789ac6095f236412370b11365e5f12d1
Gene C <arch@sapience.com>
2023-01-02 09:24:59 -0500

    Fix bug with some multipart mime email from some reporters

--- src/dmarc_report/lib/email.py
+++ src/dmarc_report/lib/email.py
@@ -3,6 +3,7 @@ Handles exatrction of mime attachments
 """
 import os
 import email
+import mimetypes
 from .utils import get_glob_file_list
 
 def email_file_list(topdir):
@@ -26,11 +27,40 @@ def email_extract_attachment(topdir, email_file, keep_email, skip_exist=True):
 
     if email_str:
         mail = email.message_from_string(email_str)
-        attach_fname = mail.get_filename()
-        attach_path = os.path.join(topdir, attach_fname)
-        if not (skip_exist and os.path.exists(attach_path)):
-            with open(attach_path, 'wb') as fobj:
-                fobj.write(mail.get_payload(decode=True))
+
+        # handle multipart
+        if mail.is_multipart():
+            counter = 1
+            for part in mail.walk():
+                # multipart/* are just containers
+                if part.get_content_maintype() == 'multipart':
+                    continue
+                # Applications should really sanitize the given filename so that an
+                # email message can't be used to overwrite important files
+                attach_fname = part.get_filename()
+                if not attach_fname:
+                    ext = mimetypes.guess_extension(part.get_content_type())
+                    if not ext:
+                        # Use a generic bag-of-bits extension
+                        ext = '.bin'
+                    attach_fname = f'part-{counter:03d}{ext}'
+
+                # skip txt, html and bin
+                if ext in ['.txt', '.html', '.bin'] :
+                    continue
+
+                counter += 1
+                attach_path = os.path.join(topdir, attach_fname)
+                if not (skip_exist and os.path.exists(attach_path)):
+                    with open(attach_path, 'wb') as fobj:
+                        fobj.write(part.get_payload(decode=True))
+
+        else:
+            attach_fname = mail.get_filename()
+            attach_path = os.path.join(topdir, attach_fname)
+            if not (skip_exist and os.path.exists(attach_path)):
+                with open(attach_path, 'wb') as fobj:
+                    fobj.write(mail.get_payload(decode=True))
         if not keep_email:
             os.unlink(email_path)
 
